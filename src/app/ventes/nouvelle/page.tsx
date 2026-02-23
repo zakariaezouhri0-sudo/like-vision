0ad7c18e8a9358d0d4ecba7609d03d174e070e67
zakariae zouhri <zakariaezouhri0@gmail.com>
2026-02-23 17:20:47 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'na

--- src/app/ventes/nouvelle/page.tsx
+++ src/app/ventes/nouvelle/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useRouter } from "navigation";
+import { useRouter } from "next/navigation";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
